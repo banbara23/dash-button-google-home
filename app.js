47cfe40e38709657bd00603132ea4d7cc4687def
ikemura <8417910+banbara23@users.noreply.github.com>
2018-04-06 12:19:53 +0900

    作業中だが一旦コミット

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const shell = require('shelljs')
-dash_button = require('node-dash-button');
-var dash = dash_button("b4:7c:9c:4e:2f:fd", null, null, 'all'); //address from step above
-dash.on("detected", function (){
-  console.log("call sh");
-  shell.exec('/home/pi/sound/play.sh')
-});
+const dash_button = require('node-dash-button');
+
+// const dash = dash_button("b4:7c:9c:4e:2f:fd", null, null, 'all');
+
+// dash.on("detected", function (){
+//   console.log("call sh");
+//   shell.exec('/home/pi/dev/dash-button-google-home/play.sh')
+// });
+
+shell.exec(`aplay --quiet ${process.cwd()}/play.sh`) //テンプレートリテラル
+console.log(process.cwd()) //プロジェクトパスを取得できる /Users/ikemura/dev/dash-button-google-home
---
+++ paly.sh
@@ -0,0 +1,7 @@
+# curl "https://api.voicetext.jp/v1/tts" \
+#      -o "voice.wav" \
+#      -u "bx3eyggxff8rlcmh:" \
+#      -d "text=ねぇグーグル、動物のこえ" \
+#      -d "speaker=hikari"
+
+aplay --quiet /home/pi/dev/dash-button-google-home/voice.wav
