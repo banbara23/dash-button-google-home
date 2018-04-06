0036e8499a6843010836520eb571fc633064b47c
ikemura <8417910+banbara23@users.noreply.github.com>
2018-04-06 12:32:15 +0900

    add package vicetext

---
+++ speek.js
@@ -0,0 +1,22 @@
+//http://alice345.hatenablog.com/entry/2014/07/16/001218
+const fs = require('fs')
+const VoiceText = require('voicetext')
+const voice = new VoiceText('bx3eyggxff8rlcmh:')
+
+voice
+  .speaker(voice.SPEAKER.HIKARI)
+  .emotion(voice.EMOTION.HAPPINESS)
+  .emotion_level(voice.EMOTION_LEVEL.HIGH)
+  .volume(200)
+  .speak('ねぇグーグル、動物のこえ', function(e, buf){
+    if(e){
+      console.error(e);
+      return;
+    }
+    fs.writeFile('./voice.wav', buf, 'binary', function(e){
+      if(e){
+        console.error(e);
+        return;
+      }
+    });
+  });
